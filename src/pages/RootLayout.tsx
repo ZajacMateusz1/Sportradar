24b1f4857a6e510e5a6b8c835d3359baa9f6f28e
Mateusz Zajac <144244766+ZajacMateusz1@users.noreply.github.com>
2026-03-21 21:58:07 +0100

    Install Tailwind CSS, add wrapper, import fonts

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -1,11 +1,11 @@
 import { Outlet } from "react-router-dom";
 export default function RootLayout() {
   return (
-    <>
+    <div className="min-h-screen max-w-270 mx-auto px-4">
       <header></header>
       <main>
         <Outlet />
       </main>
-    </>
+    </div>
   );
 }
