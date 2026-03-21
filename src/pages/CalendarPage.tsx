779f11ca605058918d917125ba3db0aad2ee5abb
Mateusz Zajac <144244766+ZajacMateusz1@users.noreply.github.com>
2026-03-21 13:34:09 +0100

    Add eventsMap for O(1) lookup and render events.

--- src/pages/CalendarPage.tsx
+++ src/pages/CalendarPage.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
+import EVENTS from "../utils/events.json";
 
 export default function CalendarPage() {
   const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
@@ -21,7 +22,16 @@ export default function CalendarPage() {
   const [currentYear, setCurrentYear] = useState(currentDate.getFullYear());
   const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
   const firstDay = new Date(currentYear, currentMonth, 1).getDay();
-
+  const eventsMap = useMemo(() => {
+    const dateToEvents: Record<string, typeof EVENTS.data> = {};
+    EVENTS.data.forEach((event) => {
+      if (!dateToEvents[event.dateVenue]) {
+        dateToEvents[event.dateVenue] = [];
+      }
+      dateToEvents[event.dateVenue].push(event);
+    });
+    return dateToEvents;
+  }, []);
   return (
     <>
       <h1>Sports Events Calendar</h1>
@@ -36,11 +46,21 @@ export default function CalendarPage() {
         </div>
         <div className="monthDays">
           {Array.from({ length: firstDay }).map((_, index) => (
-            <span key={`empty=${index}`}></span>
+            <span key={`empty-${index}`}></span>
           ))}
-          {Array.from({ length: daysInMonth }).map((_, index) => (
-            <span key={index + 1}>{index + 1}</span>
-          ))}
+          {Array.from({ length: daysInMonth }).map((_, index) => {
+            const day = index + 1;
+            const month = currentMonth + 1;
+            const date = `${currentYear}-${month < 10 ? `0${month}` : month}-${day < 10 ? `0${day}` : day}`;
+            return (
+              <span key={day}>
+                {day}
+                {eventsMap[date]
+                  ? eventsMap[date].map((event) => event.homeTeam?.name)
+                  : ""}
+              </span>
+            );
+          })}
         </div>
       </div>
     </>
