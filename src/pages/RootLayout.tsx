1228836123d63cb45a10c181bd159e885a712594
Mateusz Zajac <144244766+ZajacMateusz1@users.noreply.github.com>
2026-03-23 20:42:24 +0100

    Add navigation styles

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -1,32 +1,39 @@
 import { Outlet, NavLink } from "react-router-dom";
 export default function RootLayout() {
   return (
-    <div className="min-h-screen max-w-270 mx-auto px-4">
-      <header>
-        <nav>
-          <ul>
-            <li>
-              <NavLink
-                to="/"
-                className={({ isActive }) => (isActive ? "underline" : "")}
-              >
-                Calendar
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/events/add-event"
-                className={({ isActive }) => (isActive ? "underline" : "")}
-              >
-                Add Event Page
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
+    <>
+      <header className="bg-main-bg text-xs text-font-secondary">
+        <div className="header-wrapper flex items-center justify-between p-3 max-w-2xl mx-auto md:text-md lg:text-lg xl:text-xl sm:p-4">
+          <h1 className="font-header uppercase">Sport Event Calendar</h1>
+          <nav>
+            <ul className="flex gap-0.5 sm:gap-1 md:gap-4">
+              <li>
+                <NavLink
+                  to="/"
+                  className={({ isActive }) =>
+                    `${isActive ? "text-main" : ""} p-0.5 hover:text-main transition`
+                  }
+                >
+                  Calendar
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/events/add-event"
+                  className={({ isActive }) =>
+                    `${isActive ? "text-main" : ""} p-0.5 hover:text-main transition`
+                  }
+                >
+                  Add event
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </header>
-      <main>
+      <main className="max-w-270 mx-auto">
         <Outlet />
       </main>
-    </div>
+    </>
   );
 }
