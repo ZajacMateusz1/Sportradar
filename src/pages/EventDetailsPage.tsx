65dbb68675e4f2b2e26275812b13539ade6586fc
Mateusz Zajac <144244766+ZajacMateusz1@users.noreply.github.com>
2026-03-25 16:54:29 +0100

    Change Button to LinkButton

--- src/pages/EventDetailsPage.tsx
+++ src/pages/EventDetailsPage.tsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import LinkButton from "../components/shared/LinkButton";
 import EventsContext from "../store/events-context";
-import Button from "../components/shared/Button";
 export default function EventDetailsPage() {
   const { events } = useContext(EventsContext);
   const params = useParams();
@@ -10,7 +10,7 @@ export default function EventDetailsPage() {
     return <h2>Event not found</h2>;
   }
   return (
-    <main className="flex justify-center">
+    <div className="flex justify-center">
       <div className="event-card p-6 w-full max-w-md text-center rounded-2xl shadow-lg bg-white">
         <header className="event-info-header mb-5">
           <h2 className="text-md font-bold md:text-xl lg:text-2xl xl:text-3xl">
@@ -20,7 +20,7 @@ export default function EventDetailsPage() {
             {selectedEvent.competitionName}
           </p>
         </header>
-        <section className="event-info pb-2">
+        <section className="event-info pb-4">
           <h3 className="font-semibold text-md md:text-lg lg:text-xl xl:text-2xl">
             Details
           </h3>
@@ -31,10 +31,13 @@ export default function EventDetailsPage() {
             Time: {selectedEvent.date}, {selectedEvent.time.slice(0, 5)}
           </p>
         </section>
-        <Button>
-          <Link to="/">Back to calendar</Link>
-        </Button>
+        <LinkButton
+          className="p-1.5 rounded-lg cursor-pointer text-sm bg-main-bg text-font-secondary md:max-w-md md:text-md lg:text-lg xl:text-xl md:p-2 hover:text-main transition"
+          to="/"
+        >
+          Back to calendar
+        </LinkButton>
       </div>
-    </main>
+    </div>
   );
 }
