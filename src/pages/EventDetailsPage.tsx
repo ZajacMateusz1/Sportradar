85d99e1321131a52581da0222df23e00746afe13
Mateusz Zajac <144244766+ZajacMateusz1@users.noreply.github.com>
2026-03-25 11:31:40 +0100

    Add styles to EventDetailsPage, update id generation method in transformRawData

--- src/pages/EventDetailsPage.tsx
+++ src/pages/EventDetailsPage.tsx
@@ -4,11 +4,33 @@ import EventsContext from "../store/events-context";
 export default function EventDetailsPage() {
   const { events } = useContext(EventsContext);
   const params = useParams();
-  const findedEvent = events.find((event) => event.id === params.eventId);
-
+  const selectedEvent = events.find((event) => event.id === params.eventId);
+  if (!selectedEvent) {
+    return <h2>Event not found</h2>;
+  }
   return (
-    <main>
-      <p>{findedEvent?.homeTeamName}</p>
+    <main className="flex justify-center">
+      <div className="event-card p-6 w-full max-w-md text-center rounded-2xl shadow-lg bg-white">
+        <header className="event-info-header mb-5">
+          <h2 className="text-md font-bold md:text-xl lg:text-2xl xl:text-3xl">
+            {selectedEvent.homeTeamName} vs {selectedEvent.awayTeamName}
+          </h2>
+          <p className="text-sm md:text-md lg:text-lg xl:text-xl">
+            {selectedEvent.competitionName}
+          </p>
+        </header>
+        <section className="event-info">
+          <h3 className="font-semibold text-md md:text-lg lg:text-xl xl:text-2xl">
+            Details
+          </h3>
+          <p className="text-sm md:text-md lg:text-lg xl:text-xl">
+            Sport: {selectedEvent.sport}
+          </p>
+          <p className="text-sm md:text-md lg:text-lg xl:text-xl">
+            Time: {selectedEvent.date}, {selectedEvent.time.slice(0, 5)}
+          </p>
+        </section>
+      </div>
     </main>
   );
 }
--- src/utils/transformRawData.ts
+++ src/utils/transformRawData.ts
@@ -1,9 +1,8 @@
-import { v4 as uuidv4 } from "uuid";
 import type { EventType, TransformedEventType } from "./types";
 
 const transformRawEventData = (events: EventType[]): TransformedEventType[] => {
   const transformedEvents: TransformedEventType[] = events.map((event) => ({
-    id: uuidv4(),
+    id: `${event.dateVenue}-${event.originCompetitionId}-${event.homeTeam?.name || "Unknown"}-${event.awayTeam?.name || "Unknown"}`,
     sport: event.sport,
     date: event.dateVenue,
     time: event.timeVenueUTC,
